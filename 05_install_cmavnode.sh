4e344f582c6c717205a25fc0d5da41bf804d47f7
terial <nkariger@gmail.com>
2017-08-30 18:42:10 -0700

    Update 05_install_cmavnode.sh

--- 05_install_cmavnode.sh
+++ 05_install_cmavnode.sh
@@ -44,9 +44,85 @@ make
 #sudo make install
 
 # Copy cmavnode, config file sample and start script to $INSTALL_DIR/cmavnode
-cp /home/$INSTALL_USER/GitHub/cmavnode/build/cmavnode $INSTALL_DIR/cmavnode/cmavnode
-cp cmavnode.txt
+mkdir $INSTALL_DIR/cmavnode
+cp /home/$INSTALL_USER/GitHub/cmavnode/build/cmavnode $SETUP_DIR/cmavnode/cmavnode
 
+cat > $INSTALL_DIR/cmavnode/cmavnode.conf.sample << \EOF
+#[aseriallink]
+#    type=serial
+#    port=/dev/ttyUSB0
+#    baud=57600
 
+[audplink]
+    type=udp
+    targetip=127.0.0.1
+    targetport=14559
+    localport=14550
+    sim_enable=true
+    sim_packet_loss=25
+    packet_drop_enable=true
 
+[audplink2]
+    type=udp
+    targetip=127.0.0.1
+    targetport=14555
+    localport=14556
+EOF
+
+# Copy preconfigured mavlink-router.conf to INSTALL_DIR/mavink-router/
+cp $SETUP_DIR/cmavnode.txt /boot/cmavnode.txt 
+
+# Create cmavnode start script
+cat > $INSTALL_DIR/cmavnode/start_cmavnode.sh << \EOF
+#!/bin/bash
+#
+
+# cmavnode
+CMAVNODE_DIR=/opt/cmavnode
+CMAVNODE_LOG=/opt/log/services
+
+# Get cmavnode.txt from /boot and conver to CMAVNODE_DIR/cmavnode.conf
+DETECT_CONF=`ls /boot | grep -c cmavnode.txt`
+if [ "$DETECT_CONF" == "0" ]; then
+echo "No configuration file found for mavlink-router!"
+exit 1
+else
+echo "Getting configuration file.."
+dos2unix -n /boot/cmavnode.txt $CMAVNODE_DIR/cmavnode.conf
+fi
+
+# Start Mavlink-router
+$CMAVNODE_DIR/cmavnode -i -f $CMAVNODE_DIR/cmavnode.conf > $CMAVNODE_LOG/start_cmavnode.log 2>&1
+EOF
+
+# add execute permissions to start_cmavnode.sh
+chmod +x $INSTALL_DIR/$CMAVNODE_DIR/start_cmavnode.sh
+
+# Create directory for mavlink-router dataflash logs
+   if [ ! -d /opt/log ]; then
+   echo "No existing log directory"
+   else
+   mkdir /opt/log
+   fi
+
+# Create systemd unit file
+cat > /etc/systemd/system/cmavnoder.service << \EOF
+[Unit]
+Description=CMAVNode
+
+[Service]
+Type=simple
+ExecStart=/opt/cmavnode/start_cmavnode.sh
+ExecReload=/bin/kill -HUP $MAINPID
+Restart=on-failure
+
+[Install]
+WantedBy=multi-user.target
+Alias=cmavnode.service
+EOF
+
+ Create symlink and reload systemctl and start mavink-router
+systemctl daemon-reload
+systemctl start cmavnode.service
+systemctl status cmavnode.service -l
 
